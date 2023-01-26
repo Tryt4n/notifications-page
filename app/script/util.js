43bff1fe71b0101e0e380b63b2a2e73fab960419
Tryt4n <78303570+Tryt4n@users.noreply.github.com>
2023-01-26 16:22:06 +0100

    Delete app directory

--- app/script/util.js
+++
@@ -1,9 +0,0 @@
-export default function preventByClickingLinks() {
-  const links = document.querySelectorAll("a");
-  links.forEach((link) =>
-    link.addEventListener("click", (e) => {
-      e.preventDefault(); // by default this has to be removed to possible click in the links
-      e.stopPropagation();
-    })
-  );
-}
